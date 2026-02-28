1e68f027ff16d182926ba89d202162a828dca229
M Surya Teja <43179774+msuryateja37@users.noreply.github.com>
2026-02-28 09:44:13 +0530

    Fix port number handling in app.listen

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(Number(process.env.PORT) || 3000);
 }
 bootstrap().catch((err) => {
   console.error(err);
